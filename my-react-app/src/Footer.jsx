18524d78c10dc1fdc5aae6ef341a87e457b3cd01
ielin <agentstarkvip@gmail.com>
2024-05-22 16:47:53 +0400

    Created new React App.
    Testing.

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer()
+{
+return(
+    <footer>&copy; {new Date().getFullYear()} 21 Candles Studios</footer>
+);
+}
+
+export default Footer
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,25 @@
+function Header() {
+  return (
+    <header>
+      <h1>This is the Header.</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Designs</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header
