f0aac4a790a391a855739b8dff6f8e195ad792ae
ielin <agentstarkvip@gmail.com>
2024-05-26 00:18:23 +0400

    Started NetNinja tutorial.

--- my-react-app/src/Home.jsx
+++ my-react-app/src/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Home() {
+  const [firstName, setFirstName] = useState('Joel');
+  const [age, setAge] = useState(50);
+
+  function handleClick() {
+    console.log("Button Clicked");
+    setFirstName("Ellie");
+    setAge(14);
+  }
+
   return (
-    <div className='home'>
-        <h2>Home</h2>
+    <div className="home">
+      <h2>Home</h2>
+      <p>{firstName} is {age} years old.</p>
+      <button onClick={handleClick}>Click Here</button>
     </div>
-  )
+  );
 }
